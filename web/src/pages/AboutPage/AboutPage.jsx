7387c966a95b603d887ebd6289eb917a09718bf0
Leimomi Bong <leimomibong@gmail.com>
2024-11-06 06:15:49 -1000

    Your comment

---
+++ web/src/pages/AboutPage/AboutPage.jsx
@@ -0,0 +1,15 @@
+import { Metadata } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <Metadata title="About" description="About page" />
+
+      <p>
+        This page is for About kind of things. Stay tuned for more to come. Maybe something vaguely interesting.
+      </p>
+    </>
+  )
+}
+
+export default AboutPage
